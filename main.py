51f4fe6abff8ac85310617f5448bc272d3ea7564
elorm-11 <elormkovey@gmail.com>
2024-12-04 19:39:43 +0000

    1 commit

---
+++ main.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue","red"}
+sety = {"blue", "yellow","red","black"}
+print("orinal set elements:")
+print(setx)
+print(sety)
+print("\nInersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
